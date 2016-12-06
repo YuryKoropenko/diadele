932c430bc460ec12cdc22381d33cc01bf79882ad
Yury Koropenko <yurykoropenko@gmail.com>
2016-12-06 14:00:22 +0200

    second commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,17 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+		/*______Slider______*/
+	$(".slider__block").slick({
+		dots: true
+	});
+	/*_____Выпадающее меню_____*/
+	$(".nav__rings").on("click", function() {
+		$(".popup").hide();
+		$(".rings").stop(false, true).fadeToggle(300);
+		return false
+	});
+	$(".nav__earrings").on("click", function() {
+		$(".popup").hide();
+		$(".earrings").stop(false, true).fadeToggle(300);
+		return false
+	});
+});
